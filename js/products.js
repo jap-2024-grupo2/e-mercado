d9551e94b48957be4dfb2a8bc9b3290c0d4e0edb
ValeHernandezz <valehernandezmodino@gmail.com>
2024-08-16 17:05:00 -0300

    Arreglos estilos lista de productos y login

--- js/products.js
+++ js/products.js
@@ -1,48 +1,50 @@
-fetch("https://japceibal.github.io/emercado-api/cats_products/101.json")
-.then(res => res.json())
-.then(response => {
-    console.log(response)
+fetch('https://japceibal.github.io/emercado-api/cats_products/101.json')
+  .then((res) => res.json())
+  .then(({ products, catName }) => {
+    console.log(products)
 
-    const categoryTitle = document.getElementById("categoryTitle")
-    categoryTitle.textContent = response.catName
+    const categoryTitle = document.getElementById('categoryTitle')
+    categoryTitle.textContent = catName
 
-    const productsContainer = document.getElementById("productsContainer")
+    const productsContainer = document.getElementById('productsContainer')
 
     const fragment = document.createDocumentFragment()
 
-    response.products.forEach(product => {
+    products.forEach(
+      ({ image, name, description, currency, cost, soldCount }) => {
+        const card = document.createElement('div')
+        card.classList.add('card', 'm-4')
+        card.style = 'width: 24rem;'
 
-        const card = document.createElement("div")
-        card.classList.add("card")
+        const img = document.createElement('img')
+        img.classList.add('card-img-top')
+        img.src = image
+        img.alt = `Imagen de ${name}`
 
-        const image = document.createElement("img")
-        image.classList.add("card-img-top")
-        image.src = product.image
+        const cardBody = document.createElement('div')
+        cardBody.classList.add('card-body')
 
-        const cardBody = document.createElement("div")
-        cardBody.classList.add("card-body")
+        const cardTitle = document.createElement('h5')
+        cardTitle.classList.add('card-title', 'card-title-h5')
+        cardTitle.textContent = name
 
-        const cardTitle = document.createElement("h5")
-        cardTitle.classList.add("card-title")
-        cardTitle.textContent = product.name
+        const cardDescription = document.createElement('p')
+        cardDescription.classList.add('card-text', 'card-description')
+        cardDescription.textContent = description
 
-        const cardDescription = document.createElement("p")
-        cardDescription.classList.add("card-text")
-        cardDescription.textContent = product.description
+        const cardPrice = document.createElement('p')
+        cardPrice.classList.add('card-text', 'card-price')
+        cardPrice.textContent = `${currency} ${cost}`
 
-        const cardPrice = document.createElement("p")
-        cardPrice.classList.add("card-text")
-        cardPrice.textContent = `${product.currency} ${product.cost}` 
-
-        const cardSoldCount = document.createElement("p")
-        cardSoldCount.classList.add("card-text")
-        cardSoldCount.textContent = `${product.soldCount} vendidos`
+        const cardSoldCount = document.createElement('p')
+        cardSoldCount.classList.add('card-text', 'card-sold-count')
+        cardSoldCount.textContent = `${soldCount} vendidos`
 
         cardBody.append(cardTitle, cardDescription, cardPrice, cardSoldCount)
-        card.append(image, cardBody)
+        card.append(img, cardBody)
         fragment.appendChild(card)
-    });
+      }
+    )
 
     productsContainer.appendChild(fragment)
-})
-
+  })
