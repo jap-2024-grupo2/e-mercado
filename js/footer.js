bddabefa3392b6fe12f1b757cdf263b8e55a87dd
ValeHernandezz <valehernandezmodino@gmail.com>
2024-11-09 18:04:03 -0300

    fix: Problemas de rutas imágenes footer

--- js/footer.js
+++ js/footer.js
@@ -10,19 +10,19 @@ document.addEventListener('DOMContentLoaded', function () {
     {
       name: 'Kelly Salas',
       link: 'https://github.com/ksalas039',
-      imgSrc: '../img/kelly-profile.png',
+      imgSrc: '/img/kelly-profile.png',
       altText: 'Kelly Salas'
     },
     {
       name: 'Fernanda Rigali',
       link: 'https://github.com/FerRigali',
-      imgSrc: '../img/fernanda-profile.jpg',
+      imgSrc: '/img/fernanda-profile.jpg',
       altText: 'Fernanda Rigali'
     },
     {
       name: 'Ailen Albornoz',
       link: 'https://github.com/AilenAlbornoz',
-      imgSrc: '../img/ailen-profile.jpeg',
+      imgSrc: '/img/ailen-profile.jpeg',
       altText: 'Ailen Albornoz'
     }
   ]
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', function () {
           <p class="text-muted">Este sitio web forma parte de</p>
           <a href="https://tu-enlace.com" target="_blank" rel="noopener noreferrer">
             <img 
-              src="../img/jap-logo.png" 
+              src="/img/jap-logo.png" 
               alt="Imagen del footer" 
               class="img-fluid" 
               style="max-width: 250px; transition: transform 0.3s ease; transform: scale(1);" 
